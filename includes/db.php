c25ba8f2c596a8fea8b4e5386d8b2f45939ed5d6
Ikhsan <pratamaikhsan974.com>
2025-05-19 19:08:21 +0800

    commit 4

--- includes/db.php
+++ includes/db.php
@@ -1,11 +1,13 @@
 <?php
-$host = "q0wk40ksgs448kg80884wk44";
-$user = "voting"; // sesuaikan dengan user database kamu
-$pass = "pemilihan123";     // sesuaikan dengan password database kamu
-$db   = "pemilihan_ketua";
+// Ambil host dari environment variable atau default ke 'localhost'
+$host = getenv('DB_HOST') ?: 'q0wk40ksgs448kg80884wk44'; // 'mysql-db' = nama container MySQL
+$user = getenv('DB_USER') ?: 'voting';
+$pass = getenv('DB_PASS') ?: 'pemilihan123';
+$db   = getenv('DB_NAME') ?: 'pemilihan_ketua';
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conn) {
     die("Koneksi database gagal: " . mysqli_connect_error());
 }
+?>
