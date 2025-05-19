f6f7e5fc5e0422c491d4240853b31ee2f4399405
Ikhsan <pratamaikhsan974.com>
2025-05-19 19:28:56 +0800

    commit 8

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,6 @@
 <?php
 $host = getenv('DB_HOST') ?: 'q0wk40ksgs448kg80884wk44';  // 'mysql' = nama container database di Coolify
-$user = getenv('DB_USER') ?: 'voting';
+$user = getenv('DB_USER') ?: 'mysql';
 $pass = getenv('DB_PASS') ?: 'pemilihan123';
 $db   = getenv('DB_NAME') ?: 'pemilihan_ketua';
 
