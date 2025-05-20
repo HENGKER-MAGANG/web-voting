e311ce3117f73f248982bfa04c8ccd498143d2a1
Ikhsan <pratamaikhsan974.com>
2025-05-20 08:57:05 +0800

    commit 13

--- admin/hapus_anggota.php
+++ admin/hapus_anggota.php
@@ -1,12 +1,12 @@
 <?php
-include '../includes/db.php';
-include '../includes/auth.php';
-cekLogin('admin');
+include '../koneksi.php';
+$id = $_GET['id'];
 
-if (isset($_GET['id'])) {
-    $id = intval($_GET['id']);
-    mysqli_query($conn, "DELETE FROM anggota WHERE id = $id");
-}
+// Hapus data di tabel suara terlebih dahulu
+mysqli_query($koneksi, "DELETE FROM suara WHERE id_anggota = '$id'");
 
-header("Location: dashboard.php");
-exit;
+// Lalu hapus data anggota
+mysqli_query($koneksi, "DELETE FROM anggota WHERE id = '$id'");
+
+header("location:data_anggota.php?pesan=hapus_sukses");
+?>
